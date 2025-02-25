21c44e1d8c922f40a3fc664b3b1ca22eb6d884d1
Travis Trout <ttroutindy@gmail.com>
2025-02-24 20:54:24 -0500

    Initial WPF Project

---
+++ src/Raven.App.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Raven.App.Wpf.ViewModel;
+using System.Windows;
+
+namespace Raven.App.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainViewModel();
+
+            DataContext = _viewModel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+    }
+}
